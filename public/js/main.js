4c12345a6d92629b0fe1083db79eff3c8c6feb1c
Terrence <parliamentaryterrence@gmail.com>
2013-09-26 20:12:49 -0600

    initial commit, before any messing about

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,8 @@
-
+angular.module("CPCCA", [])
+  .config(["$location", "$routeProvider", function($location, $routeProvider){
+    $location.html5Mode(true).hashPrefix("!");
+    $routeProvider
+      .when("/", {
+        template: "<div>BLAH</div>"
+      })
+  }])
