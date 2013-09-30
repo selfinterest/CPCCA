7547b8b96d54e48a9357a51d936fc300ea13c9ad
Terrence <parliamentaryterrence@gmail.com>
2013-09-30 14:58:55 -0600

    Configuration

---
+++ private/config.php
@@ -0,0 +1,13 @@
+<?php
+$configurations = array(
+    "local" => array(
+        "connection" => 'mysql:host=localhost;dbname=antisemitism',
+        "username" => 'antis3mite',
+        "password" => 'antis3mite'
+    ),
+    "remote" => array(
+        "connection" => 'mysql:host=localhost;dbname=combatin_antisemitism',
+        "username" => 'combatin_antisem',
+        "password" => "}Qvfh4I!XUDg"
+    )
+);
